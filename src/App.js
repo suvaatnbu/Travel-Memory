48a289887a95ce6327863d1b4a3a5f2234fd043c
suvabrata <suvaatnbu@gmail.com>
2025-04-12 22:23:21 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// client/src/App.js
+import React from 'react';
+import './App.css';
+function App() {
+  return (
+    <div>
+      <h1>Welcome To Brainware University 🌍</h1>
+    </div>
+  );
+}
+
+export default App;
